4f1a3c93b11ff214d7b8daffed7c3a4ffa01e876
KoriAntunez <kori.antunez@unmsm.edu.pe>
2022-12-16 13:30:02 -0500

    commit

---
+++ petsociety/src/AppRouter.js
@@ -0,0 +1,5 @@
+import { useEffect } from "react";
+import { Routes, Route, useLocation } from "react-router-dom";
+// Validación de Rutas
+import PublicRoute from "./routers/PublicRoute";
+import PrivateRoute from "./routers/PrivateRoute";
